fa17a5bdf9ffe3468712a1852e579e37fb018779
Nathan Holland <nholland94@gmail.com>
2020-01-28 14:03:44 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import emoji
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def helloIndex():
+    return emoji.emojize('Hello World! :globe_with_meridians:')
+
+if __name__ == '__main__':
+    app.run(
+        ssl_context = ('secrets/cert.pem', 'secrets/key.pem'),
+        host = '0.0.0.0',
+        port = 81
+    )
